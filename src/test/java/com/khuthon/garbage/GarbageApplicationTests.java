152ce978294383a7d11be7566694ae592eaafe71
SangYeong Park <tkddud386@gmail.com>
2025-05-09 18:13:41 +0900

    initial commit

---
+++ src/test/java/com/khuthon/garbage/GarbageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khuthon.garbage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
